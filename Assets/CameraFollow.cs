9bfbce6a638bdea0ef47cb11a6cbd3153471ee19
Forrest Smith <fsmith2017@gmail.com>
2018-01-28 14:49:03 -0500

    Base Game Complete
    Base game of randomly generated green bounce pads completed. TODO: Alternative bounce pads, collectibles (powerups, score supplements), and dynamically generated bounce pads.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothSpeed = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.position.y > transform.position.y)
+        {
+            //transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            //transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed * Time.deltaTime);
+
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    float movement = 1f;
+    public float movementSpeed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
